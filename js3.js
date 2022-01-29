e2efac65e1f76ccaa224ca50598304acffed2c0a
Jyldyzbek Shatmanov <88623161+jyldyzbek1@users.noreply.github.com>
2022-01-29 18:07:07 +0600

    Delete js3.js

--- js3.js
+++
@@ -1,5 +0,0 @@
-// упр-3
-
-let userName = "Jyldyzbek";
-let userMail = "jyldyzbek.shatmanov.kk@gmail.com";
-document.write(`<br>Имя пользователя: ${userName} <br> Почта пользователя: ${userMail}`);
